9f743af297fd55173a0af1171ef9f63307f8151e
supernovahs <harshitsinghal252@gmail.com>
2022-11-01 16:35:42 +0530

    Support for address and uint type

---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+export default function Header() {
+  return (
+    <div><ConnectButton/></div>
+  )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,22 +5,18 @@ import { RainbowKitProvider, getDefaultWallets } from '@rainbow-me/rainbowkit';
 import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
+import Layout from '../components/Layout';
+import { ChakraProvider } from '@chakra-ui/react'
 
 const { chains, provider, webSocketProvider } = configureChains(
   [
-    chain.mainnet,
-    chain.polygon,
-    chain.optimism,
-    chain.arbitrum,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true'
-      ? [chain.goerli, chain.kovan, chain.rinkeby, chain.ropsten]
-      : []),
+    chain.mainnet
   ],
   [
     alchemyProvider({
       // This is Alchemy's default API key.
       // You can get your own at https://dashboard.alchemyapi.io
-      apiKey: '_gg7wSSi0KMBsdKnGVfHDueq6xMB9EkC',
+      apiKey: 'gDhsVUBEe61W2Q0w40A7Jwr3ZVyJ_Mvo',
     }),
     publicProvider(),
   ]
@@ -40,11 +36,15 @@ const wagmiClient = createClient({
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <ChakraProvider>
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider chains={chains}>
+        <Layout>
         <Component {...pageProps} />
+        </Layout>
       </RainbowKitProvider>
     </WagmiConfig>
+    </ChakraProvider>
   );
 }
 
