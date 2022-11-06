8e75099b180f8fcbca6dd6b1bf4703b1846f8ce7
supernovahs <harshitsinghal252@gmail.com>
2022-11-07 02:31:18 +0530

    nodemailer support

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,17 @@ const cron = require("node-cron");
 const { MongoClient } = require("mongodb");
 const { join } = require("path");
 const ethers = require("ethers");
+const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_MAINNET);
-
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_PASSWORD
+    }
+  });
 // ...
 
 // Schedule tasks to be run on the server.
@@ -17,32 +24,46 @@ const update = async () => {
     console.log("process",process.env.DATABASE_URI);
     const uri = process.env.DATABASE_URI;
     const client = new MongoClient(uri);
-    var query = { email: "harshitsinghal252@gmail.com"};
     const database = client.db('sloot');
     const fields = database.collection('slot');
-    console.log("fields",fields);
+    
     fields.find({}).toArray(async function(err,field){
         if (err) throw err;
-        console.log("field",field);
-        console.log("fields length",field.length);
+       
         let a;
         for(let i =0; i< field.length; i++){
-            console.log("field i " , field[i]);
+          
              a = await UpdateSlot(field[i].previous,field[i].address,field[i].slot,field[i].type);
             console.log("a",a);
+            if(a !=0 ){
+                console.log("CHANGE IN SLOT!!!!!!!");
+                // Update db
+                let newval = {$set: {email:field[i].email,address:field[i].address,slot:field[i].slot,previous:a,type:field[i].type}};
+                fields.updateOne(field[i],newval,function(err,re){
+                    if(err) throw err;
+                    console.log("Updated successfully");
+                })
+                // notify by email
+        
+                let messageOptions = {
+                    from: process.env.EMAIL,
+                    to: 'supernovahs@proton.me',
+                    subject: 'Scheduled Email',
+                    text: 'Hi there. This email was automatically sent by us.'
+                  };
+                  
+                  
+                  transporter.sendMail(messageOptions, function(error, info) {
+                    if (error) {
+                      throw error;
+                    } else {
+                      console.log('Email successfully sent!');
+                    }
+                  });
+        
+            }
         }
-       
-
-     if(a !=0 ){
-        // Update db
-        let newval = {$set: {email:field.email,address:field.address,slot:field.slot,previous:a,type:field.type}};
-        fields.updateOne(field,newval,function(err,re){
-            if(err) throw err;
-            console.log("Updated successfully");
-        })
-        // notify by email
-
-    }
+     
     });
 
 }
@@ -50,7 +71,7 @@ update();
 
 const UpdateSlot = async (previous,address,slot,Type) =>{
     console.log("address",address);
-    let res = await provider.getStorageAt(address,0);
+    let res = await provider.getStorageAt(address,ethers.BigNumber.from(slot).toHexString());
     if(Type == "address"){
         console.log("address ok");
         let z = ethers.utils.defaultAbiCoder.decode(["address"],res);
